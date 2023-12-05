ab65b23432140163b500859dfed5640b3d2c4355
ThaisRozas <thaisrozas@gmail.com>
2023-12-05 15:48:09 -0300

    fix: rename algorithm from random search to local random search

---
+++ part-1/local-random-search-1.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def objective_function(x):
+    return x[0]**2 + x[1]**2
+
+def is_valid(x, xl, xu):
+    return xl <= x[0] <= xu and xl <= x[1] <= xu
+
+def random_search(Nmax, xl, xu):
+    xbest = np.random.uniform(low=xl, high=xu, size=2)
+    fbest = objective_function(xbest)
+    sigma = find_optimal_sigma(xbest, xl, xu)
+    
+    for i in range(Nmax):
+        n = np.random.normal(0, sigma, size=2)
+        xcand = xbest + n
+        
+        if is_valid(xcand, xl, xu):
+            fcand = objective_function(xcand)
+            
+            if fcand < fbest:
+                xbest = xcand
+                fbest = fcand
+    
+    return xbest, fbest, sigma
+
+def find_optimal_sigma(x, xl, xu):
+    sigma_values = np.linspace(0.01, 1.0, 100)
+    best_value = float('inf')
+    best_sigma = None
+    
+    for sigma in sigma_values:
+        n = np.random.normal(0, sigma, size=2)
+        xcand = x + n
+        
+        if is_valid(xcand, xl, xu):
+            fcand = objective_function(xcand)
+            
+            if fcand < best_value:
+                best_value = fcand
+                best_sigma = sigma
+    
+    return best_sigma
+
+# Executar 100 rodadas do algoritmo
+num_rounds = 100
+Nmax_per_round = 1000
+xl = -100.0
+xu = 100.0
+
+best_solutions = []
+best_values = []
+best_sigmas = []
+
+for _ in range(num_rounds):
+    best_solution, best_value, best_sigma = random_search(Nmax_per_round, xl, xu)
+    best_solutions.append(best_solution)
+    best_values.append(best_value)
+    best_sigmas.append(best_sigma)
+
+# Encontrar a melhor solução entre todas as rodadas
+index_of_best_solution = np.argmin(best_values)
+overall_best_solution = best_solutions[index_of_best_solution]
+overall_best_value = best_values[index_of_best_solution]
+min_value_optimal_solution = min(best_sigmas)
+print("Menor valor que encontra a solução ótima:", min_value_optimal_solution)
+
+# Criar um gráfico 3D para visualizar a função e as melhores soluções
+x1_range = np.linspace(xl, xu, 100)
+x2_range = np.linspace(xl, xu, 100)
+x1_mesh, x2_mesh = np.meshgrid(x1_range, x2_range)
+f_values = objective_function([x1_mesh, x2_mesh])
+
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(x1_mesh, x2_mesh, f_values, cmap='viridis', alpha=0.8)
+ax.set_xlabel('x1')
+ax.set_ylabel('x2')
+ax.set_zlabel('Valor da Função Objetivo')
+ax.set_title('Função Objetivo em Projeção 3D')
+
+best_solutions = np.array(best_solutions)
+ax.scatter(best_solutions[:, 0], best_solutions[:, 1], best_values, c='red', marker='x', label='Melhores Soluções')
+
+ax.scatter(overall_best_solution[0], overall_best_solution[1], overall_best_value, c='blue', marker='*', s=200, label='Melhor Solução Geral')
+ax.legend()
+
+plt.show()
